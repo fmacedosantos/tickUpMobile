83f6b9374ff7395530fee58c87f2fc33f34bc6e0
fmacedosantos <felipemacedo986@gmail.com>
2024-05-26 15:49:01 -0300

    Finalizando os logins.

--- app/src/main/java/com/example/tickup/AdminLogin.java
+++ app/src/main/java/com/example/tickup/AdminLogin.java
@@ -2,13 +2,111 @@ package com.example.tickup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class AdminLogin extends AppCompatActivity {
+    private EditText inputEmailContato, inputIdEvento;
+    private Button btnEntrar;
+    private OkHttpClient client;
+    private JSONObject jsonObject;
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login);
+
+        inputEmailContato = findViewById(R.id.inputEmailContato);
+        inputIdEvento = findViewById(R.id.inputIdEvento);
+        btnEntrar = findViewById(R.id.btnEntrar);
+
+        client = new OkHttpClient();
+        jsonObject = new JSONObject();
+        url = "https://tick-up-1fb4969b94c5.herokuapp.com/api/Evento/Login";
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailContato = inputEmailContato.getText().toString();
+                String idEvento = inputIdEvento.getText().toString();
+
+                try {
+                    jsonObject.put("emailContato", emailContato);
+                    jsonObject.put("idEvento", idEvento);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), jsonObject.toString());
+                Request request = new Request.Builder()
+                        .url(url)
+                        .post(body)
+                        .build();
+
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        e.printStackTrace();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(AdminLogin.this, "Erro ao fazer login", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        if (response.isSuccessful()) {
+                            String responseData = response.body().string();
+                            boolean loggedIn = Boolean.parseBoolean(responseData);
+                            if (loggedIn) {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(AdminLogin.this, "Login bem-sucedido", Toast.LENGTH_SHORT).show();
+
+                                        Intent intent = new Intent(AdminLogin.this, VerifyTicket.class);
+                                        startActivity(intent);
+                                    }
+                                });
+                            } else {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(AdminLogin.this, "Credenciais inválidas", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+                        } else {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(AdminLogin.this, "Erro: " + response.message(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+        });
     }
 }
--- app/src/main/java/com/example/tickup/UserLogin.java
+++ app/src/main/java/com/example/tickup/UserLogin.java
@@ -2,13 +2,112 @@ package com.example.tickup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class UserLogin extends AppCompatActivity {
+    private EditText inputEmail, inputSenha;
+    private Button btnEntrar;
+    private OkHttpClient client;
+    private JSONObject jsonObject;
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+
+        inputEmail = findViewById(R.id.inputEmailUsuario);
+        inputSenha = findViewById(R.id.inputSenhaUsuario);
+        btnEntrar = findViewById(R.id.btnEntrarUsuario);
+
+        client = new OkHttpClient();
+        jsonObject = new JSONObject();
+        url = "https://tick-up-1fb4969b94c5.herokuapp.com/api/Usuario/Login";
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = inputEmail.getText().toString();
+                String senha = inputSenha.getText().toString();
+
+                try {
+                    jsonObject.put("email", email);
+                    jsonObject.put("senha", senha);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), jsonObject.toString());
+                Request request = new Request.Builder()
+                        .url(url)
+                        .post(body)
+                        .build();
+
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        e.printStackTrace();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(UserLogin.this, "Erro ao fazer login", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        if (response.isSuccessful()) {
+                            String responseData = response.body().string();
+                            boolean loggedIn = Boolean.parseBoolean(responseData);
+                            if (loggedIn) {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(UserLogin.this, "Login bem-sucedido", Toast.LENGTH_SHORT).show();
+
+                                        Intent intent = new Intent(UserLogin.this, MyTickets.class);
+                                        intent.putExtra("emailUsuario", email);
+                                        startActivity(intent);
+                                    }
+                                });
+                            } else {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(UserLogin.this, "Credenciais inválidas", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+                        } else {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(UserLogin.this, "Erro: " + response.message(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+        });
     }
 }
