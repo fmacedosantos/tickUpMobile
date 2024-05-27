82757ac18e92c202f93e7405f5707d3a7c3bc1a1
fmacedosantos <felipemacedo986@gmail.com>
2024-05-27 12:36:30 -0300

    Adicionando a dependência gson.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,7 @@ android {
 dependencies {
 
     implementation ("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation ("com.google.code.gson:gson:2.8.8")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
