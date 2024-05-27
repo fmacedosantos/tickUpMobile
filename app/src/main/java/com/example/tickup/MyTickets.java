9691fe755dc32dd90b8233ce4f27dc300e73f03c
fmacedosantos <felipemacedo986@gmail.com>
2024-05-27 12:35:40 -0300

    Iniciando o desenvolvimento da lista de ingressos.

--- app/src/main/java/com/example/tickup/MyTickets.java
+++ app/src/main/java/com/example/tickup/MyTickets.java
@@ -1,14 +1,20 @@
 package com.example.tickup;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyTickets extends AppCompatActivity {
+    ListView listaIngressos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_tickets);
+
+        listaIngressos = findViewById(R.id.listaIngressos);
+
     }
 }
