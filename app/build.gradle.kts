22ef82a39bbb16dc3c2b6b03bb97e931742d9cd1
fmacedosantos <felipemacedo986@gmail.com>
2024-05-28 16:03:49 -0300

    Alterando a dependência para trabalhar com máscaras.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,8 @@ dependencies {
     implementation ("com.squareup.okhttp3:okhttp:4.9.3")
     implementation ("com.google.code.gson:gson:2.8.8")
 
+    implementation ("com.github.santalu:maskara:1.0.0")
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
