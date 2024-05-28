4b13a6df35d26279cd69ff0365e05a405479179e
fmacedosantos <felipemacedo986@gmail.com>
2024-05-28 12:35:35 -0300

    Alterando MyTickets.

---
+++ app/src/main/java/com/example/tickup/Ingresso.java
@@ -0,0 +1,22 @@
+package com.example.tickup;
+
+public class Ingresso {
+    private String idIngresso;
+    private String nomeEvento;
+
+    public String getIdIngresso() {
+        return idIngresso;
+    }
+
+    public void setIdIngresso(String idIngresso) {
+        this.idIngresso = idIngresso;
+    }
+
+    public String getNomeEvento() {
+        return nomeEvento;
+    }
+
+    public void setNomeEvento(String nomeEvento) {
+        this.nomeEvento = nomeEvento;
+    }
+}
--- app/src/main/java/com/example/tickup/MyTickets.java
+++ app/src/main/java/com/example/tickup/MyTickets.java
@@ -3,11 +3,25 @@ package com.example.tickup;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MyTickets extends AppCompatActivity {
-    ListView listaIngressos;
+    private ListView listaIngressos;
+    private String email, url;
+    private OkHttpClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +29,19 @@ public class MyTickets extends AppCompatActivity {
         setContentView(R.layout.activity_my_tickets);
 
         listaIngressos = findViewById(R.id.listaIngressos);
+        email = getIntent().getStringExtra("emailUsuario");
+        url = "https://tick-up-1fb4969b94c5.herokuapp.com/api/Compra/Usuario/" + email;
+
 
     }
+
+    private void obterIngressos(){
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+
+
+    }
+
 }
