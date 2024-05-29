88124d102303f01a3a33883aaf00471437a4b7b4
fmacedosantos <felipemacedo986@gmail.com>
2024-05-29 14:54:30 -0300

    Redirecionando o usuário cadastrado.

--- app/src/main/java/com/example/tickup/RegisterUser.java
+++ app/src/main/java/com/example/tickup/RegisterUser.java
@@ -2,6 +2,7 @@ package com.example.tickup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -201,6 +202,10 @@ public class RegisterUser extends AppCompatActivity {
                                 @Override
                                 public void run() {
                                     Toast.makeText(RegisterUser.this, "Usuário registrado com sucesso", Toast.LENGTH_SHORT).show();
+
+                                    Intent intent = new Intent(RegisterUser.this, MyTickets.class);
+                                    intent.putExtra("emailUsuario", email);
+                                    startActivity(intent);
                                 }
                             });
                         } else {
